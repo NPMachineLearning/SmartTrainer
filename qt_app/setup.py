f22a53cd707d5e1fdcd909de6d731031c75e82d0
nelson punch <tomeno2004@gmail.com>
2024-06-07 18:11:50 +0800

    update setup.py and document

--- qt_app/setup.py
+++ qt_app/setup.py
@@ -4,15 +4,17 @@ import os
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 build_options = {'packages': [],
-                 'includes': ['rep_counting'], 
+                 'includes': [], 
                  'excludes': [],
+                 # including rep_counting source code
+                 # and config file
                  'include_files': ['../rep_counting', '../smart_trainer_config'],
                  'include_path': os.path.abspath("../")}
 
 base = 'gui'
 
 executables = [
-    Executable('test_app.py', base=base)
+    Executable('app.py', base=base)
 ]
 
 setup(name='Exercise Repetition Counter',
